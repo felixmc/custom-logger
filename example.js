d70a91f1039a0076a25104963d0a239d1a45ea96
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-06-03 00:34:52 +0100

    Update example to show message formatting with util.format

--- example.js
+++ example.js
@@ -3,7 +3,15 @@ var log = require('./logger').config({ level: 0 });
 log.debug('hello?');
 
 log.info('hello world!');
-	
+
 log.warn('careful there, world');
-	
+
 log.error('WHOA WHOA WHOA world!');
+
+log.info("I see", "3", "bananas!") // Outputs "I see 3 bananas!"
+
+log.config({
+	messageFormatting: true
+});
+
+log.info("I see %d %s!", 3, "bananas"); // Outputs "I see 3 bananas!"
